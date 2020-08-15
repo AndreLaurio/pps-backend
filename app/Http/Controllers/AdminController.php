0ebd663d4c65ed8fdfeac8bc1fe71b5139701ea8
Hyperstorm321 <45072321+Hyperstorm321@users.noreply.github.com>
2020-08-15 09:18:11 -0800

    [UPDATE] Admin create users

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //request comes here
+}
