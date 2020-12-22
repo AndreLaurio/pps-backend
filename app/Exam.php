467dc5cedf925a32ad40872236733a24dcfbbe1e
Joshua Mamangun <mamangunjoshua@gmail.com>
2020-12-22 16:53:24 +0800

    Updated Admin Exams Pagination

--- app/Exam.php
+++ app/Exam.php
@@ -36,8 +36,7 @@ class Exam extends Model
                                 DB::raw('DATE_FORMAT(updated_on, "%Y %b %d, %h:%i %p") AS updated_on_f'))
                     ->where('is_active', true)
                     // ->order_by('exam_title', 'ASC')
-                    ->paginate(1);
-
+                    ->paginate(5);
         return $query;
     }
 
