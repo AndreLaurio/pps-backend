70f0375f09503ee122ae2e0b955cfb1d5819319e
Andre Laurio <45072321+Hyperstorm321@users.noreply.github.com>
2021-05-03 22:28:46 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,5 +7,5 @@ style="position: absolute;
     margin-left: -50px;
     width: 100px;
     height: 100px;">
-    <button onclick="location.href = 'http://localhost:8080';"  class="proceed" style="padding: 14px 40px; color: white; display: inline-block; text-align: center; border: none; background-color: indigo;  ">PROCEED</button>
+    <button onclick="location.href = 'http://localhost:8081';"  class="proceed" style="padding: 14px 40px; color: white; display: inline-block; text-align: center; border: none; background-color: indigo;  ">PROCEED</button>
 </div>
